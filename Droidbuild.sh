3f50ef8a1f2c85cafd77e9d377dea255dc3d6f3e
Andrewerr <toddot@protonmail.com>
2022-09-05 19:36:19 +0300

    [droidbuild] [hotdogb]: Initial bring-up

---
+++ Droidbuild.sh
@@ -0,0 +1,22 @@
+setup_target hotdogb
+
+target_hotdogb(){
+  LOCAL_PATH=$(dirname $BASH_SOURCE)
+  include $BASEDIR/droidbuild/build_vars.sh
+  TARGET_NEEDS_RESYNC=true
+  TARGET_LOCAL_MANIFESTS+=$LOCAL_PATH/manifests/hotdogb.xml
+  TARGET_ARCH_BITNESS=64
+  TARGET_CODENAME="hotdogb"
+  TARGET_FULLNAME="OnePlus 7T"
+  TARGET_BUILDTYPE="user"
+  TARGET_SIGNED_BUILD=true
+  TARGET_HOSTS_CAT=1
+  TARGET_SUPPORTS_MICROG=0
+  TARGET_SUPPORTS_GMSCOMPAT=1
+  target_build-device
+}
+
+droidbuild_module(){
+  # Dummy
+  echo 1 > /dev/null
+}
