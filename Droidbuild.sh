5be8cbf8eb9bc25a72b19f0f4b07d26f27211930
Andrewerr <toddot@protonmail.com>
2023-07-12 12:33:07 +0200

    [hotdogb] [manifests] [droidbuild]: initial thunderpluck promotion

--- Droidbuild.sh
+++ Droidbuild.sh
@@ -1,6 +1,6 @@
 setup_target hotdogb
 
-target_hotdogb(){
+target_hotdogb() {
   LOCAL_PATH=$(dirname $BASH_SOURCE)
   include $BASEDIR/droidbuild/build_vars.sh
   TARGET_NEEDS_RESYNC=true
@@ -8,15 +8,14 @@ target_hotdogb(){
   TARGET_ARCH_BITNESS=64
   TARGET_CODENAME="hotdogb"
   TARGET_FULLNAME="OnePlus 7T"
-  TARGET_BUILDTYPE="user"
+  TARGET_BUILDTYPE="eng"
   TARGET_SIGNED_BUILD=true
   TARGET_HOSTS_CAT=2
-  TARGET_SUPPORTS_MICROG=0
   TARGET_SUPPORTS_GMSCOMPAT=1
   target_build-device
 }
 
-droidbuild_module(){
+droidbuild_module() {
   # Dummy
-  echo 1 > /dev/null
+  echo 1 >/dev/null
 }
